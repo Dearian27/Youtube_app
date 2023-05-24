320f00f55b130dd10403424ab5e6c4e6137614a2
Den <dearian2706@gmail.com>
2023-05-24 18:49:19 +0300

    change axios config

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -14,11 +14,14 @@ const instance = axios.create({
   },
 })
 
-instance.interceptors.response.use(response => {
-  return response;
-}, error => {
-  return Promise.reject(error);
-});
+instance.interceptors.request.use((config) => {
+  config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+  return config;
+},
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance
 
